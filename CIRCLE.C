923aca7084e1109a9939099fc0c2e15f5687b1d9
Bhanu Pratap Singh <65491251+Bhanupsingh2414@users.noreply.github.com>
2020-08-24 14:21:27 +0530

    Add files via upload
    Mid Point Circle Implementation.

---
+++ CIRCLE.C
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<graphics.h>
+void drawcircle(int x0,int y0,int radius)
+{
+	int x=radius;
+	int y=0;
+	int err=0;
+	while(x>=y)
+	{
+	putpixel(x0+x,y0+y,7);
+	putpixel(x0+y,y0+x,7);
+	putpixel(x0-y,y0+x,7);
+	putpixel(x0-x,y0+y,7);
+	putpixel(x0-x,y0-y,7);
+	putpixel(x0-y,y0-x,7);
+	putpixel(x0+y,y0-x,7);
+	putpixel(x0+x,y0-y,7);
+	if(err<=0)
+	{
+		y+=1;
+		err+=2*y+1;
+	}
+	if(err>0)
+	{
+		x-=1;
+		err-=2*x+1;
+	}
+	}
+}
+void main()
+{
+	int gdriver=DETECT, gmode, error,x,y,r;
+	initgraph(&gdriver,&gmode,"C:\\tc\\bgi");
+	printf("enter radius of circle: ");
+	scanf("%d",&r);
+	printf("enter cordinates of center(x and y): ");
+	scanf("%d%d",&x,&y);
+	drawcircle(x,y,r);
+	getch();
+}
